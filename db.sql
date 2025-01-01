d6564775e63450a0d109ca16c5558069f7699458
meriem el mecaniqui <melecaniqui@gmail.com>
2025-01-01 21:40:27 +0100

    ajouter table ban and favoris

--- db.sql
+++ db.sql
@@ -22,3 +22,53 @@ create table games(
     user_id bigint references users(user_id) 
 
 );
+-- critique:
+CREATE TABLE reviews (
+    review_id INT AUTO_INCREMENT PRIMARY KEY,        
+    user_id INT NOT NULL,                     
+    game_id INT NOT NULL,                     
+             
+    review TEXT,                            
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
+    FOREIGN KEY (user_id) REFERENCES users(user_id) ,
+    FOREIGN KEY (game_id) REFERENCES games(game_id) 
+);
+-- bibliotheques
+CREATE TABLE user_games (
+    id INT AUTO_INCREMENT PRIMARY KEY,         
+    user_id INT,                               
+    game_id INT,                               
+    status ENUM('en_cours', 'termine', 'abandonné') NOT NULL,  
+                
+    play_time INT,                            
+    is_favorite BOOLEAN,                       
+   added_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,  
+   
+    FOREIGN KEY (user_id) REFERENCES users(user_id),  
+    FOREIGN KEY (game_id) REFERENCES games(game_id)    
+);
+-- chat_messages
+create TABLE chat_messages{
+    chat_id INT AUTO_INCREMENT PRIMARY KEY,
+     user_id INT, 
+     message TEXT,
+     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+       FOREIGN KEY (user_id) REFERENCES users(user_id)
+}
+CREATE TABLE favorite_games (
+   favorite _id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT,         
+    game_id INT,       
+  
+    FOREIGN KEY (user_id) REFERENCES users(user_id),
+    FOREIGN KEY (game_id) REFERENCES games(game_id)
+);
+CREATE TABLE bans (
+    bans_id INT AUTO_INCREMENT PRIMARY KEY,  
+    user_id INT NOT NULL,                    
+    banned_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,  
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+  
+);
+
+
